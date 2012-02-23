130d6ba27cc54ae468993e41f19af0b093b60c44
Jonathan Cutrer <joncutrer@gmail.com>
2012-02-23 13:38:31 +0000

    ProTemplateCompiler

---
+++ Lib/ProTemplateCompiler.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Copyright 2010-2011, PRONIQUE Software (http://pronique.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2011-2012, PRONIQUE Software (http://pronique.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+/**
+ * ProUtils Plugin
+ * 
+ * ProTemplateCompiler Class
+ *
+ * This class uses regex to prepare and compile text templates
+ * 
+ * @package ProUtils
+ * @subpackage ProUtils.Lib
+ */
+class ProTemplateCompiler {
+    protected $template;
+    protected $rules = array();
+    protected $tag = '{$tag}';
+    
+    public function __construct( $template='', $rules='', $tag='' ) {
+        $this->template = $template;
+        if (!empty( $tag ) ) {
+            $this->tag = $tag;    
+        }
+        if( !empty( $rules ) && is_array( $rules ) ) {
+            $this->rules = $rules;
+            $this->prepare();
+        }
+    }
+  
+    protected function prepare( ) {
+        $tmpl = $this->template;
+        foreach( $this->rules as $key=>$rule ) {
+            $tmpl = preg_replace_callback(
+                $rule['match'],
+                'ProTemplateCompiler::replaceWithVariableCallback', 
+                $tmpl
+            );               
+        }     
+        $this->template = $tmpl;
+    } 
+    
+    /**
+    * Takes associative array in, $data 
+    * replaces template tags with match keys in data
+    * and returns the compiled template as text
+    * 
+    * @param mixed $vars
+    * @return string
+    */
+    public function compile( $dataArr ) {
+        $compiled_template = $this->template;
+        foreach( $dataArr as $key=>$val ) {
+            $compiled_template = preg_replace(
+                "/{\\$" . preg_quote($key) . "}/",
+                $val, 
+                $compiled_template
+            );             
+        }     
+        return $compiled_template;
+    }
+    
+    protected function replaceWithVariableCallback( $matches ) {
+        //TODO Add support for custom Tags
+        return $matches[1] . '{$' . $matches[2] . '}' . $matches[4];
+    }    
+}
